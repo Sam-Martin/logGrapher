784a163fe2818d92b74fde3db9e93ab8cf0eb427
Sam Martin <smartin@sdl.com>
2013-03-10 11:39:56 +0000

    Added compression of hash variable string

--- js/main.js
+++ js/main.js
@@ -2,42 +2,72 @@
   var hostAverage
   var perVM
 
-      function sortByTimestamp(a, b) {
-          if (a.x < b.x) return -1;
-          if (a.x > b.x) return 1;
-          return 0;
-      }
+function sortByTimestamp(a, b) {
+	if (a.x < b.x) return -1;
+	if (a.x > b.x) return 1;
+	return 0;
+}
+function load()
+{
+	//Get your own Browser API Key from  https://code.google.com/apis/console/
+	gapi.client.setApiKey('AIzaSyARN29uPAYghHG7TLcdvHu5Jp1b5ryobtc');
+	gapi.client.load('urlshortener', 'v1');
+
+}
+window.onload = load;
 
       //objs.sort(compare);
-      $(document).ready(function () {
-
+    $(document).ready(function () {
+		
+		
+		// Enable the short url generator
+		$('#short-url').click(function(ev){
+			ev.preventDefault();
+			
+			$('#short-url').text("Please wait..."); //debug
+			// Show the Modal
+			$('#short-url-modal').modal({show:true});
+			
+			var request = gapi.client.urlshortener.url.insert({
+			  'resource': {
+			  'longUrl': location.href
+			}
+			});
+			request.execute(function(response) 
+			{
+				
+				if(response.id != null)
+				{
+					$('#short-url').text("Short URL");
+					$('#short-url-modal p').text(response.id)
+					
+				}
+				else
+				{
+					$('#short-url-modal p').text(response.error);
+				}
+			
+			});
+		});
+		
           // Check for pre-filled report
-          if (window.location.hash) {
-              var hash = window.location.hash.substring(1); //Puts hash in variable, and removes the # character
-              // loop through each value and set it in the form
-              $.each($.parseJSON(decodeURIComponent(hash)), function (index, element) {
-                  if(element.value == "on"){
-                      $('[name=' + element.name + ']').attr('checked', true);
-                  }else{
-                      $('[name=' + element.name + ']').val(element.value);
-                  }
-              });;
-
-          }
+          populateFormFromHash();
 
           $("form").submit(function (ev) {
               ev.preventDefault();
-                
-       
-              // Update the hash
-              window.location.hash = encodeURIComponent(JSON.stringify($('form').serializeArray()));
               
-              if($('#chart-height').val()!=""){
-                  
-                      $('#container').height($('#chart-height').val());
-              }
-              $('form').slideUp();
-              $("#container").html('<i class="icon-spinner icon-spin icon-large" id="loading"></i> Loading');
+			// Display Container(s)
+			$('#container,#chart-options').show(); 
+       
+            // Update the hash
+            updateHash();
+            
+			if($('#chart-height').val()!=""){
+			  
+				  $('#container').height($('#chart-height').val());
+			}
+			  $('form').slideUp();
+			  $("#container").html('<i class="icon-spinner icon-spin icon-large" id="loading"></i> Loading');
 
               if ($('#csvRaw').val().length > 0) {
                   parseCSV($('#csvRaw').val());
@@ -60,13 +90,53 @@
           });
       });
 
-  
-  
+ var updateHash = function(){
+	
+	// Stringify form
+	var hashInput = JSON.stringify($('form').serializeArray());
+	
+	var hash = Iuppiter.Base64.encode(Iuppiter.compress(hashInput),true);
+	
+	console.log("Uncompressed query length: "+hashInput.length +" chars");// debug
+	console.log("Compressed query length: "+hash.length+" chars"); //debug
+	
+	window.location.hash =  hash;
+ }
+ 
+ var populateFormFromHash = function(){
+	if (window.location.hash) {
+	
+		// Fetch Hash
+		var hash = window.location.hash.substring(1);
+		
+		// Decompress
+		hash = Iuppiter.decompress(Iuppiter.Base64.decode(Iuppiter.toByteArray(hash), true));
+		
+		// Decode JSON
+		var formElements = $.parseJSON(hash);
+		
+		// loop through each value and set it in the form
+		$.each(formElements, function (index, element) {
+			if(element.value == "on"){
+				$('[name=' + element.name + ']').attr('checked', true);
+			}else{
+				$('[name=' + element.name + ']').val(element.value);
+			}
+		});;
+
+	}
+ 
+ }
   
   var renderChart = function (series1) {
-      
-      
-      // Initialise chart
+  
+	
+     
+	// Create link to download chart data
+	$('#download-element').attr("download", "graph.json").attr('href','data:textcharset=utf-8,'+encodeURI(JSON.stringify(series1)));
+	
+	
+    // Initialise chart
     chart = new Highcharts.Chart({
         plotOptions: {
 			
@@ -76,7 +146,7 @@
                       enabled: true
                 },
 				turboThreshold: 1000000,
-				  connectNulls: true,
+				connectNulls: true,
               },
               marker: {
                   lineWidth: 1
@@ -91,13 +161,10 @@
               enabled: true
           },
           tooltip: {
-              //shared: true,
-              //crosshairs: true,
+              shared: true,
+              crosshairs: true,
 			  enabled:true,
-              formatter: function () {
-
-                  return "<strong>" + this.series.name + ":</strong>" + this.y + "<br/>" + Highcharts.dateFormat("%d/%m/%Y %H:%M:%S", parseInt(this.x));
-              }
+            
           },
           title: {
               text: 'Custom Graph'
@@ -122,7 +189,7 @@
                   }
               },
               min: 0,
-              opposite: true
+              opposite: false
           },{
               title: {
                   text: ""
@@ -143,5 +210,4 @@
           series: series1
 
       });
-	  console.log(chart);//debug
   }
